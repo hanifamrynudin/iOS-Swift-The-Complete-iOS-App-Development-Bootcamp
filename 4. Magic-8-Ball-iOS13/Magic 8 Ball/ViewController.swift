37728e2be33880d97583ee3ebd3508c105e1a684
Hanif Fadillah Amrynudin <documenthanif16@gmail.com>
2022-07-10 17:49:27 +0700

    My fourth Ios App
    This is my fourth Ios App after completed the course in section 5 : Swift Programming Challenge

---
+++ 4. Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    @IBAction func rolButtonPressed(_ sender: UIButton) {
+        let ballAray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball5")]
+        
+        ballImageView.image = ballAray[Int.random(in: 0...4)]
+   
+    }
+}
+
