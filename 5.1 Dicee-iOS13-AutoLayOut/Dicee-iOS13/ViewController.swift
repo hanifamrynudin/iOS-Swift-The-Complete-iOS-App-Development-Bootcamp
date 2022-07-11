34e24408924711d1236bd90404d00269959c6c80
Hanif Fadillah Amrynudin <documenthanif16@gmail.com>
2022-07-11 20:57:21 +0700

    My fifth.One Ios App
    This is my fifth Ios App after completed the course in section 6

---
+++ 5.1 Dicee-iOS13-AutoLayOut/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //From main(design) hold control button and drag to this code (ViewController.swift)
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        WHO           .WHAT    VALUE
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageView1.image =  diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+
+    }
+}
+
