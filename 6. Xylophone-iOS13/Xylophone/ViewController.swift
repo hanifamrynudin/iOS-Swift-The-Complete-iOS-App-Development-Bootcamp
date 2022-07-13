1254f8b30b1ac063eabb68a5eca5ed32952fa1a0
Hanif Fadillah Amrynudin <documenthanif16@gmail.com>
2022-07-13 07:02:35 +0700

    This is my sixth Ios App
    This is my sixth Ios App after completed the course in section 7

---
+++ 6. Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+
+        playSound(title: (sender.titleLabel?.text)!)
+        
+        sender.alpha = 0.5
+
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(title: String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+}
+
+
