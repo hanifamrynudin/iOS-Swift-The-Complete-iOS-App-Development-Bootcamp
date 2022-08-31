14436f9a3faf71cccea7d846c7964807a65a409f
Hanif Fadillah Amrynudin <documenthanif16@gmail.com>
2022-08-31 16:28:26 +0700

    update hidden api key

--- 13. ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ 13. ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -16,7 +16,7 @@ protocol CoinManagerDelegate {
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "339DCEC2-3E1C-433C-8FEF-39456E2B789E"
+    let apiKey = "{{API-KEY}}"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
